04246c3c9b149bf9e6f37b2669f5013f5da3c1b9
NoelPanBrea <npanbrea@gmail.com>
2026-03-02 11:34:03 +0100

    feat: FPLN

--- FPLN/Tokenize.py
+++ FPLN/Tokenize.py
@@ -13,24 +13,3 @@ def mark_tokenize(text: str) -> list[str]:
 def ngram_tokenize(text: str, n: int = 1) -> list[str]:
     pretokenized = space_tokenize(text)
     return [" ".join(pretokenized[i:i + n]) for i, _ in enumerate(pretokenized)]
-
-def feature_extraction(text: str) -> np.matrix:
-    pretokenized = space_tokenize(text)
-    input_matrix  = np.zeros((len(pretokenized), 4))
-    label_matrix = np.zeros((len(pretokenized), 1))
-    for i, e  in enumerate(pretokenized):
-        # asignamos valores feature_vector(char, is_punct, is_num, next_char)
-        feature_vector = np.zeros(4)
-        feature_vector[0] = e
-        feature_vector[3] = pretokenized[i+1]
-        if np.isin(e, list(ascii_letters + digits), invert=True):      
-            feature_vector[0] = 1
-        if e.isnumeric():
-            feature_vector[1] = 1
-        input_matrix[i, :] = feature_vector
-        # asignamos valor label (0=char no terminal, 1=char terminal)
-        if feature_vector[3] == 0:
-            label_matrix[i][0] = 1 
-    
-def classification_tokenize(text: str) -> list[str]:
-    data_matrix = feature_extraction(text)
